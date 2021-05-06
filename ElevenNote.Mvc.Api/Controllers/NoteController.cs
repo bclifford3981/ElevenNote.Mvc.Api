6e723e2abda947baf8f1915b08f11261e4db7edc
bclifford23 <73857749+bclifford23@users.noreply.github.com>
2021-05-06 15:14:37 -0400

    controller add and index.html edit

--- ElevenNote.Mvc.Api/Controllers/NoteController.cs
+++ ElevenNote.Mvc.Api/Controllers/NoteController.cs
@@ -1,4 +1,6 @@
 ﻿using ElevenNote.Models;
+using ElevenNote.Services;
+using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,29 +12,42 @@ namespace ElevenNote.Mvc.Api.Controllers
     [Authorize]
     public class NoteController : Controller
     {
-       
-        // GET: Note
+
+
         public ActionResult Index()
         {
-            var model = new NoteListItem[0];
+            var service = CreateNoteService();
+            var model = service.GetNotes();
             return View(model);
         }
-        // Add method here VVVV
-        // GET
+
         public ActionResult Create()
         {
             return View();
         }
-        // Add code here VVVV
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create(NoteCreate model)
         {
-            if (ModelState.IsValid)
+            if (!ModelState.IsValid) return View(model);
+
+            var service = CreateNoteService();
+
+            if (service.CreateNote(model))
             {
-                
-            }
+                TempData["SaveResult"] = "Note Created";
+                return RedirectToAction("Index");
+            };
+            ModelState.AddModelError("", "Note could not be created.");
             return View(model);
+        }
+
+        private NoteService CreateNoteService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new NoteService(userId);
+            return service;
         }
     }
 }
