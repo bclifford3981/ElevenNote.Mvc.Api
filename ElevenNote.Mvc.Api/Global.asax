d3c4138b57a888566b4f1347e13c439372045274
bclifford23 <73857749+bclifford23@users.noreply.github.com>
2021-05-06 11:58:31 -0400

    Add project files.

---
+++ ElevenNote.Mvc.Api/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ElevenNote.Mvc.Api.MvcApplication" Language="C#" %>
---
+++ ElevenNote.Mvc.Api/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ElevenNote.Mvc.Api.Startup))]
+namespace ElevenNote.Mvc.Api
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
