2bde866a8d5e329716fcc32cd33910540f4c90a7
bclifford23 <73857749+bclifford23@users.noreply.github.com>
2021-05-06 13:22:24 -0400

    add db and connection string and stuff

--- ElevenNote.Mvc.Api/Controllers/AccountController.cs
+++ ElevenNote.Mvc.Api/Controllers/AccountController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using ElevenNote.Mvc.Api.Models;
+using ElevenNote.Mvc.Api.Data;
 
 namespace ElevenNote.Mvc.Api.Controllers
 {
